b325d6371c6ffecbd799bc7d0727b8f2c21513d7
Lasse Rosenow <lasse.j.rosenow@gmail.com>
2024-10-11 16:59:48 +0200

    Update structure and add multiple templates as unique packages generated by a python script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,15 +1,16 @@
 # Changelog
 
-## 0.2.1
+## 0.3.0
 
 - Fixed documentation pointing to an outdated version of the template
+- Added dedicated template packages for `report`, `bachelor-thesis` and `master-thesis`
 
 ## 0.2.0
 
 - Improved Readme documentation around fonts
-- Added List of Figures
-- Added List of Tables
-- Added Listings
+- Added `List of Figures` page
+- Added `List of Tables` page
+- Added `Listings` page
 - Added abbreviations to the template
 - Fixed header showing chapter with number "0" for chapters without numbering
 
---
+++ generate_releases.py
@@ -0,0 +1,70 @@
+import tomllib
+import shutil
+import os
+
+LIB_DIR = "lib"
+TEMPLATES_DIR = "templates"
+OUTPUT_DIR = "generated"
+
+
+def read_toml(path):
+    with open(path, "rb") as f:
+        config = tomllib.load(f)
+
+    return config
+
+
+def write_toml(data, path):
+    def format_value(value):
+        """Format the value for TOML representation."""
+        if isinstance(value, str):
+            return f'"{value}"'  # Use double quotes for strings
+        elif isinstance(value, bool):
+            return "true" if value else "false"
+        else:
+            return str(value)  # Convert numbers and other types to string
+
+    with open(path, "w") as f:
+        first_row = True
+        for key, value in data.items():
+            if isinstance(value, dict):
+                f.write(f"{"" if first_row else "\n"}[{key}]\n")  # Write section header
+                for sub_key, sub_value in value.items():
+                    f.write(f"{sub_key} = {format_value(sub_value)}\n")
+            else:
+                f.write(f"{key} = {format_value(value)}\n")
+            first_row = False
+
+
+def make_package(dst: str, src: str):
+    base_config = read_toml("typst.toml")
+    version = base_config["package"]["version"]
+
+    package_config = read_toml(f"./{src}/typst.toml")
+    name = package_config["package"]["name"]
+
+    # Copy files to output folder
+    shutil.copytree(src="./lib", dst=f"{dst}/{name}/{version}")
+
+    # Merge base_config with package_config
+    merged_config = package_config
+    for item in base_config:
+        merged_config[item].update(base_config[item])
+    write_toml(merged_config, f"./{dst}/{name}/{version}/typst.toml")
+
+    # Copy license
+    shutil.copy("LICENSE", f"./{dst}/{name}/{version}/LICENSE")
+
+
+# Delete old generated releases
+if os.path.exists(f"./{OUTPUT_DIR}"):
+    shutil.rmtree(f"./{OUTPUT_DIR}")
+os.mkdir(OUTPUT_DIR)
+
+# Generate main lib
+make_package(OUTPUT_DIR, LIB_DIR)
+
+# Generate templates
+dirs = os.listdir(TEMPLATES_DIR)
+for dir in dirs:
+    make_package(OUTPUT_DIR, f"{TEMPLATES_DIR}/{dir}")
